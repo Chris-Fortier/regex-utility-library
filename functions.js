003befd37255eb1b52218c1aa7cbc66c1d35a34f
Chris-Fortier <vendor@chrisjfortier.com>
2020-05-20 09:56:41 -0700

    block out comments for the functions

--- functions.js
+++ functions.js
@@ -1,1 +1,31 @@
-// Your functions go here!
+// Match Literal Strings
+
+// Match Single Character with Multiple Possibilities
+
+// Match Single Characters Not Specified
+
+// Match Characters that Occur One or More Times
+
+// Find One or More Criminals in a Hunt
+
+// Match Beginning String Patterns
+
+// Match Ending String Patterns
+
+// Match All Letters and Numbers
+
+// Match Everything But Letters and Numbers
+
+// Match All Numbers
+
+// Match All Non-Numbers
+
+// Match Non-Whitespace Characters
+
+// Positive and Negative Lookahead
+
+// Check For Mixed Grouping of Characters
+
+// Use Capture Groups to Search and Replace
+
+// Remove Whitespace from Start and End
