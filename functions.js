a424a762b3193141c73d78b6bdf7c87d3cd53c7f
Chris-Fortier <vendor@chrisjfortier.com>
2020-05-20 16:51:07 -0700

    add lookAhead function

--- functions.js
+++ functions.js
@@ -85,6 +85,11 @@ function countNonWhiteSpace(string) {
 }
 
 // Positive and Negative Lookahead
+function lookAhead(password) {
+   // This will match passwords that are greater than 5 characters long, do not begin with numbers, and have two consecutive digits.
+   let regex = /^\D(?=\w{5})(?=\w*\d{2})/;
+   return regex.test(password);
+}
 
 // Check For Mixed Grouping of Characters
 
